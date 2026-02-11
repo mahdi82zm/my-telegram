753a6f09bed953dfcaf672d93243d37451ba33df
Mahdi <mzabihi116@gmail.com>
2026-02-11 11:52:50 +0330

    config supa

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,7 +1,16 @@
-import { createClient } from "@supabase/supabase-js";
+import { createServerClient } from "@supabase/ssr";
+import { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
 
 const Url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 
 const UnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabaseServer = createClient(Url, UnonKey);
+export const supabaseServer = (cookies: ReadonlyRequestCookies) => {
+  createServerClient(Url, UnonKey, {
+    cookies: {
+      get: (name: string) => {
+        cookies.get(name)?.value;
+      },
+    },
+  });
+};
